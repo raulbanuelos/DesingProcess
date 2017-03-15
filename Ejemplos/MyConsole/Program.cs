c3eaa64ca89b433c5e678d1f7ceb9ba240c8cc74
M0051722 <raul.banuelos.diaz@hotmail.com>
2017-03-15 15:15:08 -0600

    Librerias

---
+++ Ejemplos/MyConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Negocio> lista = new List<Negocio>();
+
+            lista.Add(new Negocio { Id = 6, Nombre = "Edgar" });
+            lista.Add(new Negocio { Id = 5, Nombre = "Raúl" });
+            lista.Add(new Negocio { Id = 3, Nombre = "José" });
+            lista.Add(new Negocio { Id = 4, Nombre = "Ramón" });
+            lista.Add(new Negocio { Id = 2, Nombre = "Rubén" });
+            lista.Add(new Negocio { Id = 1, Nombre = "Artúro" });
+            Console.WriteLine("Lista no ordenada");
+            foreach (Negocio element in lista)
+            {
+                Console.WriteLine("ID: {0} ,Nombre: {1}", element.Id, element.Nombre);
+            }
+
+            lista = lista.OrderBy(x => x.Nombre).ToList();
+
+            Console.WriteLine("Lista ordenada");
+            foreach (Negocio element in lista)
+            {
+                Console.WriteLine("ID: {0} ,Nombre: {1}", element.Id, element.Nombre);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
