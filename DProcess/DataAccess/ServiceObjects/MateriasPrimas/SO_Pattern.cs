d2d9a9f93ebd293410438d4a24e6b38614f3eb6e
M0051722 <raul.banuelos.diaz@hotmail.com>
2017-03-17 07:56:41 -0600

    Se crea el modelo de Materias Primas

---
+++ DProcess/DataAccess/ServiceObjects/MateriasPrimas/SO_Pattern.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ServiceObjects.MateriasPrimas
+{
+    public class SO_Pattern
+    {
+        #region Propiedades
+        #endregion
+
+        #region Constructores
+        #endregion
+
+        #region Métodos
+
+        public IList GetAllPattern()
+        {
+            try
+            {
+                return null;
+            }
+            catch (Exception er)
+            {
+                return null;
+            }
+
+        }
+
+        #endregion
+
+    }
+}
