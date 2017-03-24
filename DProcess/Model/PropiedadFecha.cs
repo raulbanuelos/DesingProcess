1f534a209b4ca6877d6343ab4acd53aba76417be
M0051722 <raul.banuelos.diaz@hotmail.com>
2017-03-24 08:35:02 -0600

    Se agrega la clase PropiedadFecha

---
+++ DProcess/Model/PropiedadFecha.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class PropiedadFecha
+    {
+        #region Propiedades
+        /// <summary>
+        /// Cadena que representa el nombre de la propiedad.
+        /// </summary>
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Cadena que representa una descripción larga de la propiedad.
+        /// </summary>
+        public string DescripcionLarga { get; set; }
+
+        /// <summary>
+        /// Cadena que representa una descripción corta de la propiedad.
+        /// </summary>
+        public string DescripcionCorta { get; set; }
+
+        /// <summary>
+        /// Double que representa el valor de la propiedad.
+        /// </summary>
+        public DateTime Valor { get; set; }
+        #endregion
+
+        #region Constructores
+
+        #endregion
+
+        #region Métodos
+
+        #endregion
+    }
+}
