d3cd03d60cd3333622125a109fd2dfee7ad7fcb5
M0051722 <raul.banuelos.diaz@hotmail.com>
2017-04-04 12:47:06 -0500

    Se agrega la tabla TBL_TIPO_DOCUMENTO

--- DProcess/DataAccess/ServiceObjects/ControlDocumentos/TBL_TIPO_DOCUMENTO.cs
+++ DProcess/DataAccess/ServiceObjects/ControlDocumentos/TBL_TIPO_DOCUMENTO.cs
@@ -12,29 +12,20 @@ namespace DataAccess.ServiceObjects.ControlDocumentos
     using System;
     using System.Collections.Generic;
     
-    public partial class Usuarios
+    public partial class TBL_TIPO_DOCUMENTO
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Usuarios()
+        public TBL_TIPO_DOCUMENTO()
         {
-            this.TBL_VERSION = new HashSet<TBL_VERSION>();
             this.TBL_DOCUMENTO = new HashSet<TBL_DOCUMENTO>();
         }
     
-        public string Usuario { get; set; }
-        public string Password { get; set; }
-        public string Nombre { get; set; }
-        public string APaterno { get; set; }
-        public string AMaterno { get; set; }
-        public Nullable<int> Estado { get; set; }
-        public string Usql { get; set; }
-        public string Psql { get; set; }
-        public Nullable<bool> Bloqueado { get; set; }
-        public Nullable<int> Id_Departamento { get; set; }
+        public int ID_TIPO_DOCUMENTO { get; set; }
+        public string TIPO_DOCUMENTO { get; set; }
+        public string ABREBIATURA { get; set; }
+        public Nullable<System.DateTime> FECHA_CREACION { get; set; }
+        public Nullable<System.DateTime> FECHA_ACTUALIZACION { get; set; }
     
-        public virtual TBL_DEPARTAMENTO TBL_DEPARTAMENTO { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<TBL_VERSION> TBL_VERSION { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<TBL_DOCUMENTO> TBL_DOCUMENTO { get; set; }
     }
