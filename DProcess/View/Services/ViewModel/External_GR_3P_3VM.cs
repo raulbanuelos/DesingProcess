cffe03dc65b613138f90176daae021ff64440c27
ing.practicante <ing.practicante@AG-ANDRADET-D>
2017-09-07 14:08:15 -0500

    Se agrega view model para coil

---
+++ DProcess/View/Services/ViewModel/External_GR_3P_3VM.cs
@@ -0,0 +1,117 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace View.Services.ViewModel
+{
+    public class External_GR_3P_3VM: INotifyPropertyChanged
+    {
+        #region Attributtes
+        DialogService dialog;
+        #endregion
+
+        #region INotifyPropertyChanged Métodos
+        void NotifyChange(params string[] ids)
+        {
+            if (PropertyChanged != null)
+                foreach (var id in ids)
+                    PropertyChanged(this, new PropertyChangedEventArgs(id));
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Propiedades
+        private DataTable listaHerramentales;
+        public DataTable ListaHerramentales
+        {
+            get { return listaHerramentales; }
+            set { listaHerramentales = value; NotifyChange("ListaHerramentales"); }
+        }
+
+        private double _width;
+        public double Width
+        {
+            get { return _width; }
+            set { _width = value; NotifyChange("Width"); }
+        }
+
+        private DataTable _listaOptimos;
+        public DataTable ListaOptimos
+        {
+            get { return _listaOptimos; }
+            set { _listaOptimos = value; NotifyChange("ListaOptimos"); }
+        }
+
+        private DataTable _listaMejores;
+
+        public DataTable ListaMejores
+        {
+            get { return _listaMejores; }
+            set { _listaMejores = value; NotifyChange("ListaMejores"); }
+        }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICommand BusquedaCoil
+        {
+            get
+            {
+                return new RelayCommand(parametro => BuscarExternal_GR((string)parametro));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICommand BuscarOptimos
+        {
+            get
+            {
+                return new RelayCommand(o => buscarOptimos());
+            }
+        }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="texto"></param>
+        private void BuscarExternal_GR(string texto)
+        {
+            ListaHerramentales = DataManager.GetAllEXTERNAL_GR_3P_3(texto);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void buscarOptimos()
+        {
+            ListaOptimos = new DataTable();
+            ListaMejores = new DataTable();
+
+            ListaOptimos = DataManager.GetEXTERNAL_GR_3P_3(Width);
+        }
+        #endregion
+
+        #region Constructor
+
+        public External_GR_3P_3VM()
+        {
+            BuscarExternal_GR(string.Empty);
+            dialog = new DialogService();
+        }
+        #endregion
+    }
+}
