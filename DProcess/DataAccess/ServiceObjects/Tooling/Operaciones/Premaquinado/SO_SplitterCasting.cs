6bfb3743d094628e4eb7e362354719cdc344a60f
M0051722 <raul.banuelos.diaz@hotmail.com>
2017-10-03 11:56:24 -0500

    Se nagrega metodo SetCutterSpacerS //Comentarios

--- DProcess/DataAccess/ServiceObjects/Tooling/Operaciones/Premaquinado/SO_SplitterCasting.cs
+++ DProcess/DataAccess/ServiceObjects/Tooling/Operaciones/Premaquinado/SO_SplitterCasting.cs
@@ -284,6 +284,34 @@ namespace DataAccess.ServiceObjects.Tooling.Operaciones.Premaquinado
                 return null;
             }
         }
+
+        public int SetCutterSpacerS(string codigo, double a, double b, string plano)
+        {
+            try
+            {
+                //Realizamos la conexión a través de EntityFramework.
+                using (var Conexion = new EntitiesTooling())
+                {
+                    CutterSpacerSplitter obj = new CutterSpacerSplitter();
+
+                    obj.Codigo = codigo;
+                    obj.A = a;
+                    obj.B = b;
+                    obj.Plano = plano;
+
+                    Conexion.CutterSpacerSplitter.Add(obj);
+                    Conexion.SaveChanges();
+
+                    return obj.ID_SPACER_SPLITTER;
+                }
+            }
+            catch (Exception)
+            {
+
+                return 0;
+            }
+        }
+
         #endregion
     }
 }
