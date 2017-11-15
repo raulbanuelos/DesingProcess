e7ff10bf2a81f90e53d792808e68a7518df2bc1b
ing.practicante <ing.practicante@AG-ANDRADET-D>
2017-11-15 08:59:23 -0600

    .

--- DProcess/View/Forms/Routing/WPattern.xaml.cs
+++ DProcess/View/Forms/Routing/WPattern.xaml.cs
@@ -27,7 +27,7 @@ namespace View.Forms.Routing
             InitializeComponent();
             
             CreateCylinder(new Point3D(0.2, 0.1, 0), 1.1, 1.5, 0.29, 100, Colors.SlateGray, false);
-           // CreateCylinder2(new Point3D(0, 0, 0), 0, 0.7, 0.29, 100, Colors.White, false);
+            CreateCylinder2(new Point3D(0, 0,0), 1.1,1.72, 0.17, 100, Colors.SlateGray, false);
 
         }
 
@@ -122,67 +122,67 @@ namespace View.Forms.Routing
             }
         }
 
-        //private void CreateCylinder2(Point3D center, double rin, double rout, double height, int n, Color color, bool isWireframe)
-        //{
-        //    if (n < 2 || rin == rout)
-        //        return;
-        //    double radius = rin;
-        //    if (rin > rout)
-        //    {
-        //        rin = rout;
-        //        rout = radius;
-        //    }
-        //    double h = height / 2;
-        //    Model3DGroup cylinder = new Model3DGroup();
-        //    Point3D[,] pts = new Point3D[n, 4];
-        //    for (int i = 0; i < n; i++)
-        //    {
-        //        pts[i, 0] = GetPosition(rout, i * 360 / (n - 1), h);
-        //        pts[i, 1] = GetPosition(rout, i * 360 / (n - 1), -h);
-        //        pts[i, 2] = GetPosition(rin, i * 360 / (n - 1), -h);
-        //        pts[i, 3] = GetPosition(rin, i * 360 / (n - 1), h);
-        //    }
+        private void CreateCylinder2(Point3D center, double rin, double rout, double height, int n, Color color, bool isWireframe)
+        {
+            if (n < 2 || rin == rout)
+                return;
+            double radius = rin;
+            if (rin > rout)
+            {
+                rin = rout;
+                rout = radius;
+            }
+            double h = height / 2;
+            Model3DGroup cylinder = new Model3DGroup();
+            Point3D[,] pts = new Point3D[n, 4];
+            for (int i = 0; i < n; i++)
+            {
+                pts[i, 0] = GetPosition(rout, i * 360 / (n - 1), h);
+                pts[i, 1] = GetPosition(rout, i * 360 / (n - 1), -h);
+                pts[i, 2] = GetPosition(rin, i * 360 / (n - 1), -h);
+                pts[i, 3] = GetPosition(rin, i * 360 / (n - 1), h);
+            }
 
-        //    for (int i = 0; i < n; i++)
-        //    {
+            for (int i = 0; i < n; i++)
+            {
 
-        //        for (int j = 0; j < 4; j++)
-        //            pts[i, j] += (Vector3D)center;
-        //    }
-        //    Point3D[] p = new Point3D[8];
+                for (int j = 0; j < 4; j++)
+                    pts[i, j] += (Vector3D)center;
+            }
+            Point3D[] p = new Point3D[8];
 
-        //    for (int i = 0; i < n - 1; i++)
-        //    {
+            for (int i = 0; i < n - 1; i++)
+            {
 
-        //        p[0] = pts[i, 0];
-        //        p[1] = pts[i, 1];
-        //        p[2] = pts[i, 2];
-        //        p[3] = pts[i, 3];
-        //        p[4] = pts[i + 1, 0];
-        //        p[5] = pts[i + 1, 1];
-        //        p[6] = pts[i + 1, 2];
-        //        p[7] = pts[i + 1, 3];
-        //        // Top surface:
-        //        Utility.CreateTriangleFace(p[0], p[4], p[3], color,
-        //        isWireframe, viewport2);
-        //        Utility.CreateTriangleFace(p[4], p[7], p[3], color,
-        //        isWireframe, viewport2);
-        //        // Bottom surface:
-        //        Utility.CreateTriangleFace(p[1], p[5], p[2], color,
-        //        isWireframe, viewport2);
-        //        Utility.CreateTriangleFace(p[5], p[6], p[2], color,
-        //        isWireframe, viewport2);
-        //        // Outer surface:
-        //        Utility.CreateTriangleFace(p[0], p[1], p[4], color,
-        //        isWireframe, viewport2);
-        //        Utility.CreateTriangleFace(p[1], p[5], p[4], color,
-        //        isWireframe, viewport2);
-        //        // Inner surface:
-        //        Utility.CreateTriangleFace(p[2], p[7], p[6], color,
-        //        isWireframe, viewport2);
-        //        Utility.CreateTriangleFace(p[2], p[3], p[7], color, isWireframe, viewport2);
-        //    }
-        //}
-        
+                p[0] = pts[i, 0];
+                p[1] = pts[i, 1];
+                p[2] = pts[i, 2];
+                p[3] = pts[i, 3];
+                p[4] = pts[i + 1, 0];
+                p[5] = pts[i + 1, 1];
+                p[6] = pts[i + 1, 2];
+                p[7] = pts[i + 1, 3];
+                // Top surface:
+                Utility.CreateTriangleFace(p[0], p[4], p[3], color,
+                isWireframe, viewport2);
+                Utility.CreateTriangleFace(p[4], p[7], p[3], color,
+                isWireframe, viewport2);
+                // Bottom surface:
+                Utility.CreateTriangleFace(p[1], p[5], p[2], color,
+                isWireframe, viewport2);
+                Utility.CreateTriangleFace(p[5], p[6], p[2], color,
+                isWireframe, viewport2);
+                // Outer surface:
+                Utility.CreateTriangleFace(p[0], p[1], p[4], color,
+                isWireframe, viewport2);
+                Utility.CreateTriangleFace(p[1], p[5], p[4], color,
+                isWireframe, viewport2);
+                // Inner surface:
+                Utility.CreateTriangleFace(p[2], p[7], p[6], color,
+                isWireframe, viewport2);
+                Utility.CreateTriangleFace(p[2], p[3], p[7], color, isWireframe, viewport2);
+            }
+        }
+
     }
 }
