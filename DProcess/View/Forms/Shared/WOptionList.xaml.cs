f397efd0f953663a52e71576857c97a0f14ae838
M0051722 <raul.banuelos.diaz@hotmail.com>
2017-12-14 10:35:14 -0600

    Se codifica la funcionalidad de la pantalla WOptionList.

---
+++ DProcess/View/Forms/Shared/WOptionList.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View.Forms.Shared
+{
+    /// <summary>
+    /// Lógica de interacción para WOptionList.xaml
+    /// </summary>
+    public partial class WOptionList : MetroWindow
+    {
+        public WOptionList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ DProcess/View/Services/ViewModel/ListItemViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace View.Services.ViewModel
+{
+    public class ListItemViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+        private ObservableCollection<string> _ItemsList;
+        public ObservableCollection<string> ItemsList
+        {
+            get { return _ItemsList; }
+            set { _ItemsList = value; NotifyChange("ItemsList"); }
+        }
+
+        private string _SelectedItem;
+        public string SelectedItem
+        {
+            get { return _SelectedItem; }
+            set { _SelectedItem = value; NotifyChange("SelectedItem"); }
+        }
+
+        #endregion
+
+        #region Constructors
+        public ListItemViewModel(List<string> lista)
+        {
+            ItemsList = new ObservableCollection<string>();
+            ListToObservableCollection(lista);
+        }
+
+        public ListItemViewModel(List<double> lista)
+        {
+            ItemsList = new ObservableCollection<string>();
+            ListToObservableCollection(lista);
+        }
+        #endregion
+
+        #region Methods
+
+        void ListToObservableCollection(List<double> lista)
+        {
+            foreach (double item in lista)
+                ItemsList.Add(item.ToString());
+            
+        }
+
+        void ListToObservableCollection(List<string> lista)
+        {
+            foreach (string item in lista)
+                ItemsList.Add(item);
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Métodos
+        void NotifyChange(params string[] ids)
+        {
+            if (PropertyChanged != null)
+                foreach (var id in ids)
+                    PropertyChanged(this, new PropertyChangedEventArgs(id));
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+    }
+}
