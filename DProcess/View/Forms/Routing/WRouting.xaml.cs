02453d8ca345b6530ec77b7597f67f6750a1e0c0
M0051722 <raul.banuelos.diaz@hotmail.com>
2018-02-09 08:24:59 -0600

    Se agrega la pantalla de pattern

---
+++ DProcess/View/Forms/Routing/WRouting.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MahApps.Metro.Controls;
+namespace View.Forms.Routing
+{
+    /// <summary>
+    /// Lógica de interacción para WRouting.xaml
+    /// </summary>
+    public partial class WRouting : MetroWindow
+    {
+        public WRouting()
+        {
+            InitializeComponent();
+        }
+    }
+}
