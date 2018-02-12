aeb5cdf1a379f34634b08404ce48e46db6c4d451
M0051722 <raul.banuelos.diaz@hotmail.com>
2018-02-12 09:33:01 -0600

    Se agregan las operaciones.

---
+++ DProcess/View/Services/Operaciones/Fundicion/FundicionMoldeo.cs
@@ -0,0 +1,223 @@
+﻿using Model;
+using Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View.Services.TiempoEstandar.Fundicion;
+
+namespace View.Services.Operaciones.Fundicion
+{
+    public class FundicionMoldeo : IOperacion
+    {
+        #region Propiedades de IOperacion
+        /// <summary>
+        /// Cadena que representa las instrucciones de una operación en la hoja de ruta.
+        /// </summary>
+        public string TextoProceso { get; set; }
+
+        /// <summary>
+        /// Cadena que representa las dimenciones de las herramientas de una operación en la hoja de ruta.
+        /// </summary>
+        public string TextoHerramienta { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string TextoSyteline
+        {
+            get
+            {
+                return NombreOperacion + Environment.NewLine + "MET=000 OPR= 00" + Environment.NewLine + TextoProceso + Environment.NewLine + "TOOLING" + Environment.NewLine + TextoHerramienta;
+            }
+            set
+            {
+                TextoSyteline = value;
+            }
+        }
+
+        /// <summary>
+        /// Cadena que representa el nombre de la operación en ingles.
+        /// </summary>
+        public string NombreOperacion { get; set; }
+
+        /// <summary>
+        /// Cadena que representa el número de centro de trabajo de la operación.
+        /// </summary>
+        public string CentroTrabajo { get; set; }
+
+        /// <summary>
+        /// Cadena que representa el centro de costos de la operación.
+        /// </summary>
+        public string CentroCostos { get; set; }
+
+        /// <summary>
+        /// Cadena que representa el control key de la operación, esto para control de ERP.
+        /// </summary>
+        public string ControlKey { get; set; }
+
+        /// <summary>
+        /// Entero que representa el número de operación en hoja de ruta.
+        /// </summary>
+        public int NoOperacion { get; set; }
+
+        /// <summary>
+        /// Double que presenta el tiempo maquina de la operación.
+        /// </summary>
+        public double TiempoMachine { get; set; }
+
+        /// <summary>
+        /// Double que representa el tiempo de preparación de la operación.
+        /// </summary>
+        public double TiempoSetup { get; set; }
+
+        /// <summary>
+        /// Double que representa el tiempo de trabajo hombre de la operación.
+        /// </summary>
+        public double TiempoLabor { get; set; }
+
+        /// <summary>
+        /// Booleano que representa si una operación esta en ejecucioón(Ejecutando el métod CrearOperacion())
+        /// </summary>
+        public bool IsWorking { get; set; }
+
+        /// <summary>
+        /// Collección que representa la lista de herramentales de la operación.
+        /// </summary>
+        public ObservableCollection<Herramental> ListaHerramentales { get; set; }
+
+        /// <summary>
+        /// Colección que representa todas las propiedades adquiridas en la operación.
+        /// </summary>
+        /// <example>
+        /// RPM en First Rougth Grind
+        /// </example>
+        public ObservableCollection<Propiedad> ListaPropiedadesAdquiridasProceso { get; set; }
+
+        /// <summary>
+        /// Colección que representa las alertas y posibles causas que puedan generar un mal cálculo dentro de la operación.
+        /// </summary>
+        public ObservableCollection<string> AlertasOperacion { get; set; }
+
+        /// <summary>
+        /// Colección que representa las notas de información al usuario de calculos relevantes en la operación.
+        /// </summary>
+        public ObservableCollection<string> NotasOperacion { get; set; }
+
+        /// <summary>
+        /// Colección de Materia prima que representan todas las materias primas utilizadas en la operación.
+        /// </summary>
+        public ObservableCollection<MateriaPrima> ListaMateriaPrima { get; set; }
+
+        /// <summary>
+        /// Anillo que presenta el anillo físico con las medidas que llega a la operación.
+        /// </summary>
+        public Anillo anilloProcesado { get; set; }
+
+        /// <summary>
+        /// Anillo que representa el plano ingresado por el usuario.
+        /// </summary>
+        public Anillo elPlano { get; set; }
+        #endregion
+
+        #region Métodos de IOperacion
+        /// <summary>
+        /// Método en el cual se calcula la operación.
+        /// </summary>
+        /// <param name="ElAnilloProcesado">Anillo que presenta el anillo como se recibe de la operación anterior.</param>
+        /// <param name="elPlano">Anillo que representa el plano que ingresó el usuario.</param>
+        public void CrearOperacion(Anillo ElAnilloProcesado, Anillo elPlano)
+        {
+            //Indicamos que la operación esta trabajando.
+            IsWorking = true;
+
+            //Asignamos el valor del anillor procesado al anillo de la operación.
+            anilloProcesado = ElAnilloProcesado;
+
+            double a, b, c,sec;
+            double thicknessCasting = Module.GetValorPropiedad("PattThicknessCasting", elPlano.PropiedadesAdquiridasProceso);
+            double widthCasting = Module.GetValorPropiedad("WidthCasting", elPlano.PropiedadesAdquiridasProceso);
+            a = thicknessCasting * 2.54;
+            b = widthCasting * .254;
+            c = a * b;
+            sec = Math.Round(c * 1000, 2);
+
+            //Agregamos el texto con las instrucciones de la operación.
+            TextoProceso = "SECC. TRANSV.  " + String.Format("{0:0.0}", sec) + " MM" + Environment.NewLine;
+
+            //Ejecutamos el método para calculo de Herramentales.
+            BuscarHerramentales();
+
+            //Ejecutamos el méotodo para calcular los tiempos estándar.
+            CalcularTiemposEstandar();
+
+            IsWorking = false;
+        }
+
+        public void BuscarHerramentales()
+        {
+            TextoHerramienta = "MODELO " + elPlano.Codigo + Environment.NewLine;
+        }
+
+        /// <summary>
+        /// Método en el cual se calculan los tiempos estandar.
+        /// </summary>
+        public void CalcularTiemposEstandar()
+        {
+            try
+            {
+                //Declaramos un objeto del tipo CentroTrabajo110.
+                CentroTrabajo020 objTiempos = new CentroTrabajo020();
+
+                //Ejecutamos el método para calcular los tiempos.
+                objTiempos.Calcular(elPlano);
+
+                //Mapeamos los valores correspondientes.
+                this.TiempoLabor = objTiempos.TiempoLabor;
+                this.TiempoMachine = objTiempos.TiempoMachine;
+                this.TiempoSetup = objTiempos.TiempoSetup;
+
+                //Verificamos si no se generaron alertas durante el calculo de tiempos.
+                if (objTiempos.Alertas.Count > 0)
+                {
+                    AlertasOperacion.Add("Error en calculo de tiempo estándar");
+                    AlertasOperacion.CopyTo(objTiempos.Alertas.ToArray(), 0);
+                }
+                else
+                {
+                    NotasOperacion.Add("Tiempos estándar calculados correctamente.");
+                }
+            }
+            catch (Exception er)
+            {
+                //Si ocurrio algún error, lo agregamos a la lista de alertas de la operación.
+                AlertasOperacion.Add("Error en cálculo de tiempos estándar. \n" + er.StackTrace);
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public FundicionMoldeo(Anillo plano)
+        {
+            //Asignamos los valores por default a las propiedades.
+            NombreOperacion = "FUNDICION MOLDEO";
+            CentroCostos = "32011544";
+            CentroTrabajo = "020";
+            ControlKey = "MA40";
+            elPlano = plano;
+            ListaHerramentales = new ObservableCollection<Herramental>();
+            ListaMateriaPrima = new ObservableCollection<MateriaPrima>();
+            ListaPropiedadesAdquiridasProceso = new ObservableCollection<Propiedad>();
+            NotasOperacion = new ObservableCollection<string>();
+            AlertasOperacion = new ObservableCollection<string>();
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return NombreOperacion;
+        }
+    }
+}
