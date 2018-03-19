d83c63bc9c3d4e9ba1356df4563beb466f09d29b
M0051722 <raul.banuelos.diaz@hotmail.com>
2018-03-19 12:49:43 -0600

    Se agrega la pantalla con la cual aparece una lista de usuarios con el correo electrónico de cada uno de ellos.

---
+++ DProcess/View/Services/ViewModel/UsuariosViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Model.ControlDocumentos;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace View.Services.ViewModel
+{
+    public class UsuariosViewModel : INotifyPropertyChanged
+    {
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region INotifyPropertyChanged Métodos
+        void NotifyChange(params string[] ids)
+        {
+            if (PropertyChanged != null)
+                foreach (var id in ids)
+                    PropertyChanged(this, new PropertyChangedEventArgs(id));
+        }
+        #endregion
+
+        #region Propiedades
+        private ObservableCollection<Model.ControlDocumentos.Usuarios> _ListaUsuariosCorreo;
+        public ObservableCollection<Model.ControlDocumentos.Usuarios> ListaUsuariosCorreo
+        {
+            get
+            {
+                return _ListaUsuariosCorreo;
+            }
+            set
+            {
+                _ListaUsuariosCorreo = value;
+                NotifyChange("ListaUsuariosCorreo");
+            }
+        } 
+        #endregion
+
+        #region Constructors
+        public UsuariosViewModel()
+        {
+            ListaUsuariosCorreo = DataManagerControlDocumentos.GetUsuarios();
+        } 
+        #endregion
+    }
+}
